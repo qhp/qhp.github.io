2e141444b2bcd56f7a477f8c22f317379bd3c395
Администратор <devios@my.com>
2014-11-05 08:50:17 +0200

    add retina test alert

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-$(function(){
-    var $window = $(window);alert($window.width());
+$(document).ready(function(){
+
+    var $window  = $(window);
+    var width    = $window.width();
+    var isRetina = ('devicePixelRatio' in window && window.devicePixelRatio == 2) ? true : false;
+alert(isRetina)
+    if (isRetina) {
+        replace_2x();
+    }
+
     $window.resize(function(){
 
-        var width = $window.width();
-        var isRetina = ('devicePixelRatio' in window && window.devicePixelRatio == 2) ? true : false;
-
-        if (isRetina) {
-            replace_2x();
-        }
     });
 });
 
@@ -15,9 +17,9 @@ function replace_2x() {
 
     var img_to_replace = jQuery( 'img.replace-2x' ).get();
 
-    for (var i in img_to_replace) {
+    for (var i=0,l=img_to_replace.length; i<l; i++) {
         var src = img_to_replace[i].src;
         src = src.replace(/\.(png|jpg|gif)+$/i, '@2x.$1');
         img_to_replace[i].src = src;
-    }
+    };
 }
